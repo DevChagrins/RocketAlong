3d6c9cbdb94fbb1fa993f770d49227e206bd5234
DevChagrins <malcolmjgruber@gmail.com>
2016-07-05 23:16:05 -0400

    Moving update
    Update that is to make sure latest is on GitHub before it's moved to
    GitLab

--- RocketAlong/Assets/Scripts/CameraFollowComponent.cs
+++ RocketAlong/Assets/Scripts/CameraFollowComponent.cs
@@ -6,6 +6,9 @@ public class CameraFollowComponent : MonoBehaviour
 {
     public GameObject Target = null;
     public Vector3 FollowOffset = Vector3.zero;
+    public bool IgnoreX = false;
+    public bool IgnoreY = false;
+    public bool IgnoreZ = false;
 
     private Vector3 ZOffset = Vector3.zero;
 
@@ -15,16 +18,49 @@ public class CameraFollowComponent : MonoBehaviour
         ZOffset = new Vector3(0f, 0f, transform.position.z);
 	    if(Target != null)
         {
-            transform.position = Target.transform.position - FollowOffset + ZOffset;
+            transform.position = GetTargetPosition() - FollowOffset + ZOffset;
         }
 	}
 	
 	// Update is called once per frame
 	void LateUpdate()
     {
-        if (Target != null)
+        if(Target != null)
         {
-            transform.position = Target.transform.position - FollowOffset + ZOffset;
+            transform.position = GetTargetPosition() - FollowOffset + ZOffset;
         }
 	}
+
+    Vector3 GetTargetPosition()
+    {
+        Vector3 position = Target.transform.position;
+
+        if(IgnoreX)
+        {
+            position.x = transform.position.x;
+        }
+
+        if(IgnoreY)
+        {
+            position.y = transform.position.y;
+        }
+
+        if(IgnoreZ)
+        {
+            position.z = transform.position.z;
+        }
+
+        return position;
+    }
+
+    public static Bounds GetCameraBounds()
+    {
+        Vector3 cameraBoundSize = new Vector3();
+        cameraBoundSize.x = Camera.main.orthographicSize;
+        cameraBoundSize.y = cameraBoundSize.x * (Screen.width / Screen.height);
+
+        Bounds cameraBounds = new Bounds(Camera.main.transform.position, cameraBoundSize);
+
+        return cameraBounds;
+    }
 }
